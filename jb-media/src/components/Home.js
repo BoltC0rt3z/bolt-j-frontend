bdc1450ab7bf0fc32f37cb872d4ca0a8d95b49fa
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-03-13 12:18:39 +0300

    Added styling

--- jb-media/src/components/Home.js
+++ jb-media/src/components/Home.js
@@ -3,8 +3,19 @@ import React from "react";
 import GetPosts from "./container/posts/postContainer";
 
 const Home = () => (
+  <div>
+  <div className="row">
+  <img class="img-responsive container-fluid" src="https://images.unsplash.com/photo-1519531600234-f878794d387b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"/>
+  </div>
+  <div className="container">
+    <div className="kudevide">
+    <p>All posts</p>
+    <hr/>
+    </div>
+  </div>
   <div className="container">
           <GetPosts />
+  </div>
   </div>
 );
 
